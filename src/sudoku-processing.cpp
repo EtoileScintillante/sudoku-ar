d007259dee451452a619255f8c5e374309e44fc4
Esma <esmazuurbier12@gmail.com>
2022-06-27 13:41:30 +0200

    added funct to sort cells and to create vec of grid digits

--- src/sudoku-processing.cpp
+++ src/sudoku-processing.cpp
@@ -144,3 +144,127 @@ Mat filterOutDigits(Mat image)
 
     return dst;
 }
+
+// Detect the contours of the cell and return a vector containing those contours
+// Also draws the cell contours on src image
+// Assumption: input image is image obtained through the function filterOutDigits
+std::vector< std::vector < Point > > findCells(Mat image, Mat src)
+{
+    // Invert the binary image and find contours of the cells of the grid
+    Mat invertedIMG;
+    bitwise_not(image, invertedIMG);
+    std::vector< std::vector < Point > > contours; 
+    std::vector<cv::Vec4i> hierarchy;
+    findContours(invertedIMG, contours, hierarchy, RETR_TREE, CHAIN_APPROX_SIMPLE);
+
+    // The images of the sudokus all have cells with an area between 3000 and 5000
+    // These Points will be stored in a separate vector
+    std::vector< std::vector < Point > > contoursCells;
+    for (int i = 0; i < contours.size(); i++)
+    {
+        double area = contourArea(contours[i]);
+        if (area > 3000 && area < 5000)
+        {
+            contoursCells.push_back(contours[i]);
+            drawContours(src, contours, i, Scalar(0,255,0), 2);
+        }
+    }
+
+    return contoursCells;
+}
+
+// Sort contours from left to right (used in sortCells function)
+bool sortCellsLeftToRight(std::vector<Point> a, std::vector<Point> b)
+{
+    Rect r1 = boundingRect(a);
+    Rect r2 = boundingRect(b);
+    return (r1.x < r2.x);
+}
+
+// Sort the contours of the cells from top left to bottom right
+std::vector< std::vector < Point > > sortCells(std::vector< std::vector < Point > > cells)
+{
+    // By reversing the vector, the first 9 vector<Point> belong to the cells on the top row
+    // the 9 vector<Point> after that belong to the cells on row beneath the top row, and so on.
+    // However, the vectors are not yet sorted from left to right
+    std::reverse(cells.begin(), cells.end());
+
+    // Sort the cells from left to right
+    std::vector< std::vector < Point > > sortedCells;
+    std::vector< std::vector < Point > > temp;
+    int row = 0;
+    for (int i = 0; i < 9; i++)
+    {
+        // Use temp vector to store one row (9 vector<Point>) and sort these
+        temp.clear();
+        for (int j = 0; j < 9; j++)
+        {
+            temp.push_back(cells[row+j]);
+        }
+        std::sort(temp.begin(), temp.end(), sortCellsLeftToRight);
+        // Push sorted vector<Point> back to sortedCells
+        for (int k = 0; k < 9; k++)
+        {
+            sortedCells.push_back(temp[k]);
+        }
+        row += 9; // Move to next row
+
+    }
+
+    return sortedCells;
+}
+
+// Recognize digits with the KNN model and return a 2d vector of ints
+// Input image must be image obtained from cropGrid function
+// Empty cell = 0
+std::vector< std::vector< int > > ImageToVec(Mat src, std::vector< std::vector < Point > > sortedCells, Ptr<ml::KNearest> knn)
+{
+    // Prep image
+    Mat thresh;
+    cvtColor(src, thresh, COLOR_BGR2GRAY);
+    adaptiveThreshold(thresh, thresh, 255, ADAPTIVE_THRESH_GAUSSIAN_C, THRESH_BINARY_INV, 57, 5);
+    imshow("test", thresh);
+    waitKey(0);
+
+    // Create vector to store the sudoku grid
+    std::vector< std::vector<int > > grid;
+    std::vector< int > gridRow;
+    int rowCount = 0;
+
+    // Iterate over sukdoku grid and extract digits using the KNN
+    for (int i = 0; i < 9; i++)
+    {
+        for (int k = 0; k < 9; k++)
+        {
+            Rect r = boundingRect(sortedCells[k + rowCount]);
+            Mat ROI = thresh(r); // Create image of individual cell
+            std::vector< std::vector < Point > > ROIcontours;
+            int num = 0;
+            findContours(ROI, ROIcontours, RETR_TREE, CHAIN_APPROX_SIMPLE); // Find contours of cell image
+            for (int j = 0; j < ROIcontours.size(); j++)
+            {
+                double area = contourArea(ROIcontours[j]);
+                if (area > 220) // If there is a digit in the cell
+                {
+                    Rect r1= boundingRect(ROIcontours[j]); // Create rect around the digit
+                    Mat ROI1 = ROI(r1); // Create image of the digit
+                    imshow("Digit",ROI1);
+                    waitKey(0);
+                    Mat temp1, temp2; 
+                    resize(ROI1,temp1, Size(10,10), 0,0,INTER_LINEAR );
+                    temp1.convertTo(temp2,CV_32FC1);
+                    std::vector< float > response;
+                    knn->findNearest(temp2.reshape(1,1), 1, response); // Recognize digit
+                    num = response[0];
+                }
+                
+            }
+            gridRow.push_back(num);
+        }
+        grid.push_back(gridRow); // Push back row into grid
+        gridRow.clear(); // Clear row vector 
+        rowCount+=9; // Move to next row
+    }
+
+    return grid;
+}
