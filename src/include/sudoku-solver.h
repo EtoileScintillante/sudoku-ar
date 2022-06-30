6e5495c4cd03d886f6743a4204b35bdc6b0231ba
Esma <esmazuurbier12@gmail.com>
2022-06-30 19:28:27 +0200

    added documentation

--- src/include/sudoku-solver.h
+++ src/include/sudoku-solver.h
@@ -11,10 +11,33 @@
 #include <vector>
 #include <iostream>
 
-void print_sudoku(std::vector<std::vector< int > > s);
+/**
+ * Prints the sudoku grid.
+ *
+ * @param grid 2d vector containing the sudoku grid.
+ */
+void print_sudoku(std::vector<std::vector< int > > grid);
 
-bool checkPlace(std::vector<std::vector< int > > s, int row, int col, int num);
+/**
+ * Checks whether it is safe to place a digit in a certain cell.
+ * 
+ * @param grid 2d vector containing the sudoku grid
+ * @param row row number
+ * @param col column number
+ * @param num inserted digit
+ * @return true if cell is a safe place, false otherwhise.
+ */
+bool checkPlace(std::vector<std::vector< int > > grid, int row, int col, int num);
 
-bool solve_sudoku(std::vector<std::vector< int > > s, int row, int col, std::vector<std::vector< int > > &solvedGrid);
+/**
+ * Solves the sudoku puzzle using backtracking algorithm.
+ * 
+ * @param grid 2d vector containing the sudoku grid
+ * @param row row number
+ * @param col column number
+ * @param num digit we want to insert
+ * @return true if solution to sudoku puzzle is found, false otherwhise.
+ */
+bool solve_sudoku(std::vector<std::vector< int > > grid, int row, int col, std::vector<std::vector< int > > &solvedGrid);
 
 #endif /*__SUDOKU_FUNCTIONS__*/
