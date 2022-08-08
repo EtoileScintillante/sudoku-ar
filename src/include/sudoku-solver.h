5c45a5d4c1fdf88317b00b5de18fd3ca8d071a3c
Esma <esmazuurbier12@gmail.com>
2022-08-08 09:14:26 +0200

    edited docs and function names are now camel case

--- src/include/sudoku-solver.h
+++ src/include/sudoku-solver.h
@@ -12,33 +12,33 @@
 #include <iostream>
 
 /**
- * Prints the sudoku grid.
+ * @brief Prints the sudoku grid.
  *
- * @param grid 2d vector containing the sudoku grid.
+ * @param grid 2d vector representing the sudoku grid
  */
-void print_sudoku(std::vector<std::vector< int > > grid);
+void printSudoku(std::vector<std::vector< int > > grid);
 
 /**
- * Checks whether it is safe to place a digit in a certain cell.
+ * @brief Checks whether it is safe to place a digit in a certain cell.
  * 
  * @param grid 2d vector containing the sudoku grid
  * @param row row number
  * @param col column number
  * @param num inserted digit
- * @return true if cell is a safe place, false otherwhise.
+ * @return true if cell is a safe place, false otherwhise
  */
 bool checkPlace(std::vector<std::vector< int > > grid, int row, int col, int num);
 
 /**
- * Solves the sudoku puzzle using backtracking algorithm.
+ * @brief Solves the sudoku puzzle using backtracking algorithm.
  * 
  * @param grid 2d vector containing the sudoku grid
  * @param row row number
  * @param col column number
- * @param num digit we want to insert
+ * @param num digit to insert
  * @param[out] solvedGrid 2d vector containing the solved sudoku
- * @return true if solution to sudoku puzzle is found, false otherwhise.
+ * @return true if solution to sudoku puzzle is found, false otherwhise
  */
-bool solve_sudoku(std::vector<std::vector< int > > grid, int row, int col, std::vector<std::vector< int > > &solvedGrid);
+bool solveSudoku(std::vector<std::vector< int > > grid, int row, int col, std::vector<std::vector< int > > &solvedGrid);
 
 #endif /*__SUDOKU_FUNCTIONS__*/
