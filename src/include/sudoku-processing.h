47557b2b4e5239e818b2f7e8bb7b382eb4cbe1c2
Esma <esmazuurbier12@gmail.com>
2022-08-08 09:07:26 +0200

    renamed functions that had confusing names

--- src/include/sudoku-processing.h
+++ src/include/sudoku-processing.h
@@ -12,96 +12,105 @@
 using namespace cv;
 
 /**
- * Detects a sudoku grid on an image.
+ * @brief Detects a sudoku grid on an image.
  * 
- * @param mat image with sudoku grid
- * @return vector of points representing the 4 corners of the sudoku grid.
+ * @param mat image containing a sudoku grid
+ * @return vector of points representing the 4 corners of the sudoku grid
  */
 std::vector<Point> detectGrid(Mat image);
 
 /**
- * Crops image containing a sudoku grid, so that only the grid is left over.
+ * @brief Crops image containing a sudoku grid, so that only the grid is left over.
  * 
- * @param corners vector of points representing the 4 corners of the sudoku grid.
+ * @param corners vector of points representing the 4 corners of the sudoku grid
  * @param sourceIMG original image containing the sudoku grid
- * @return cropped image of sudoku grid.
+ * @return cropped image of sudoku grid
  */
 Mat cropGrid(std::vector<Point> corners, Mat sourceIMG);
 
 /**
- * Sorts a vector of points from top left to bottom right.
+ * @brief Sorts a vector of four points from top left to bottom right.
  * 
- * @param corners vector of points representing the 4 corners of the sudoku grid.
- * @return sorted vector of points.
+ * @param corners vector of points representing the 4 corners of a square
+ * @return sorted vector of points
  */
-std::vector<Point> sortPoints4(std::vector<Point> corners);
+std::vector<Point> sortSquarePoints(std::vector<Point> corners);
 
 /**
- * Creates a binary image of the sudoku grid with the digits filtered out.
+ * @brief Creates a binary image of the sudoku grid with the digits filtered out,
+ * where the lines of the grid are black and the cells are white.
  * 
- * @param image image containing only a sudoku grid.
- * @return binary image of the sudoku grid with the digits filtered out.
+ * @param image image containing only a sudoku grid
+ * @return binary image of the sudoku grid with the digits filtered out
  */
 Mat filterOutDigits(Mat image);
 
 /**
- * Creates a binary image of the sudoku grid with the digits filtered out.
+ * @brief Finds the contours of the cells of the sudoku grid.
  * 
- * @param image binary image of the sudoku grid with the digits filtered out (must be 9x9 grid).
+ * @param image binary image of the sudoku grid with the digits filtered out
  * @param src image with sudoku grid on which the contours of the cells wll be drawn 
- * @return 2d vector of points containing the contours of the cells.
+ * @return 2d vector of points containing the contours of the cells
  */
 std::vector< std::vector < Point > > findCells(Mat image, Mat src);
 
 /**
- * Check if point a comes before point b (x coordinate of a < x coordinate of b).
+ * @brief Checks if contour a comes before contour b.
+ * It does so by adding a bounding rectangle to both contours.
+ * Contour a comes before b if the x coordinate of rectangle a is smaller than the x of rectangle b. 
  *
  * @param a vector of points
  * @param b vector of points
  * @see sortCells
  * @return true if point a comes before point b, false otherwhise
  */
-bool sortCellsLeftToRight(std::vector<Point> a, std::vector<Point> b);
+bool sortContours(std::vector<Point> a, std::vector<Point> b);
 
 /**
- * Check if point a comes before point b (x coordinate of a < x coordinate of b).
+ * @brief Sorts cells of a sudoku grid from top left to bottom right.
  *
  * @param cells 2d vector of contours of the cells of a sudoku grid (must be 81 cells)
- * @see sortCellsLeftToRight
+ * @see sortContours
  * @return sorted 2d vector of contours of the cells of a sudoku grid
  */
 std::vector< std::vector < Point > > sortCells(std::vector< std::vector < Point > > cells);
 
 /**
- * Converts an image of a sudoku grid to a 2d vector if integers.
+ * @brief Converts an image of a sudoku grid to a 2d vector of integers. 
+ * Detection of digits is achieved by using a trained KNN model.
+ * Empty cells will be represented by a zero.
  *
  * @param src cropped image of sudoku grid (must be 9x9 grid)
  * @param sortedCells sorted 2d vector of contours of the cells of a sudoku grid
- * @param knn trained K-Nearest Neighbor model to recognize the digits on the image of the sudoku grid 
+ * @param knn trained K-Nearest Neighbor model to recognize digits 
  * @see cropGrid
  * @see sortCells
- * @return 2d vector of integers representing the sudoku grid (empty cell = 0)
+ * @return 2d vector of integers representing the sudoku grid
  */
-std::vector< std::vector< int > > ImageToVec(Mat src, std::vector< std::vector < Point > > sortedCells, Ptr<ml::KNearest> knn);
+std::vector< std::vector< int > > gridToVector(Mat src, std::vector< std::vector < Point > > sortedCells, Ptr<ml::KNearest> knn);
 
 /**
- * Creates a mask for the sudoku grid (containing only the digits of cells that were originally empty).
+ * @brief Creates a mask for the sudoku grid. 
+ * This mask contains only the digits of cells that were empty in the unsolved sudoku, 
+ * since the goal of this mask is to fill in the empty cells of an unsolved sudoku image with digits
+ * and creating an image of a solved sudoku. 
+ * 
  *
  * @param contourCells sorted 2d vector of contours of the cells of a sudoku grid
  * @param solution 2d vector of integers representing the solved sudoku grid
  * @param original 2d vector of integers representing the original/unsolved sudoku grid
- * @return mask image (containing only the digits of cells that were originally empty)
+ * @return mask image 
  */
 Mat createMask(std::vector< std::vector < Point > > contoursCells, std::vector< std::vector< int > > solution, std::vector< std::vector< int > > original);
 
 /**
- * Overlays the mask on the source image to create an augmented reality effect.
+ * @brief Applies the mask to the source image to create an augmented reality effect.
  *
- * @param corners sorted vector of points representing the 4 corners of the sudoku grid.
- * @param source image on which we want to overlay the mask
- * @param mask image representing the mask 
+ * @param corners sorted vector of points representing the 4 corners of the sudoku grid
+ * @param source image to which the mask should be applied
+ * @param mask image mask 
  * @see createMask
- * @return image where the empty cells of the suoku grid contain green digits because of the mask we put over it
+ * @return image source and mask blended together, creating an image of a solved sudoku grid
  */
 Mat showSolution(std::vector< Point > corners, Mat source, Mat mask);
 
--- src/sudoku-processing.cpp
+++ src/sudoku-processing.cpp
@@ -1,7 +1,5 @@
 #include "sudoku-processing.h"
 
-// Detect the sudoku grid
-// Returns vector of points of the grid
 std::vector<Point> detectGrid(Mat image)
 {
     Mat gray, thresh;
@@ -38,8 +36,6 @@ std::vector<Point> detectGrid(Mat image)
     return approx;
 }
 
-// This function takes as input the 4 points corresponding to 4 corners of the grid
-// and it creates a cropped image containing only the grid
 Mat cropGrid(std::vector<Point> corners, Mat sourceIMG)
 {
     Point2f src_vertices[4];
@@ -61,9 +57,7 @@ Mat cropGrid(std::vector<Point> corners, Mat sourceIMG)
     return cropped;
 }
 
-// Sort points from top left (tl), top right (tr), bottom left (bl), bottom right (br)
-// Assumption: only 4 points in vector
-std::vector<Point> sortPoints4(std::vector<Point> corners)
+std::vector<Point> sortSquarePoints(std::vector<Point> corners)
 {
     std::vector<Point> sorted;
 
@@ -114,9 +108,6 @@ std::vector<Point> sortPoints4(std::vector<Point> corners)
     return sorted;
 }
 
-// Returns a binary image with the lines of the grid in white and 
-// and everything within the lines is black
-// This step is necessary for finding the contours of the cells
 Mat filterOutDigits(Mat image)
 {   
     Mat noDigits;
@@ -146,9 +137,6 @@ Mat filterOutDigits(Mat image)
     return dst;
 }
 
-// Detect the contours of the cell and return a vector containing those contours
-// Also draws the cell contours on src image
-// Assumption: input image is image obtained through the function filterOutDigits
 std::vector< std::vector < Point > > findCells(Mat image, Mat src)
 {
     // Invert the binary image and find contours of the cells of the grid
@@ -174,15 +162,13 @@ std::vector< std::vector < Point > > findCells(Mat image, Mat src)
     return contoursCells;
 }
 
-// Sort contours from left to right (used in sortCells function)
-bool sortCellsLeftToRight(std::vector<Point> a, std::vector<Point> b)
+bool sortContours(std::vector<Point> a, std::vector<Point> b)
 {
     Rect r1 = boundingRect(a);
     Rect r2 = boundingRect(b);
     return (r1.x < r2.x);
 }
 
-// Sort the contours of the cells from top left to bottom right
 std::vector< std::vector < Point > > sortCells(std::vector< std::vector < Point > > cells)
 {
     // By reversing the vector, the first 9 vector<Point> belong to the cells on the top row
@@ -202,7 +188,7 @@ std::vector< std::vector < Point > > sortCells(std::vector< std::vector < Point 
         {
             temp.push_back(cells[row+j]);
         }
-        std::sort(temp.begin(), temp.end(), sortCellsLeftToRight);
+        std::sort(temp.begin(), temp.end(), sortContours);
         // Push sorted vector<Point> back to sortedCells
         for (int k = 0; k < 9; k++)
         {
@@ -215,10 +201,7 @@ std::vector< std::vector < Point > > sortCells(std::vector< std::vector < Point 
     return sortedCells;
 }
 
-// Recognize digits with the KNN model and return a 2d vector of ints
-// Input image must be image obtained from cropGrid function
-// Empty cell = 0
-std::vector< std::vector< int > > ImageToVec(Mat src, std::vector< std::vector < Point > > sortedCells, Ptr<ml::KNearest> knn)
+std::vector< std::vector< int > > gridToVector(Mat src, std::vector< std::vector < Point > > sortedCells, Ptr<ml::KNearest> knn)
 {
     // Prep image
     Mat thresh;
@@ -265,7 +248,6 @@ std::vector< std::vector< int > > ImageToVec(Mat src, std::vector< std::vector <
     return grid;
 }
 
-// This function creates a mask which will overlay the sudoku grid in the source image (image captured by webcam/camera)
 Mat createMask(std::vector< std::vector < Point > > contoursCells, std::vector< std::vector< int > > solution, std::vector< std::vector< int > > original)
 {
     // Mask is same size as cropped image of grid
@@ -295,9 +277,6 @@ Mat createMask(std::vector< std::vector < Point > > contoursCells, std::vector< 
     return mask;
 }
 
-// This functions overlays the mask (obtained from createMask) on the source image
-// To do that it needs the corner coordinates of the grid on the source image
-// which is why we need to pass in the vector containing these corners
 Mat showSolution(std::vector< Point > corners, Mat source, Mat mask)
 {
 
